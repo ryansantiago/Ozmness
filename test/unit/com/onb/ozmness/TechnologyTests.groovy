a90d6fec2d93a697e85dd1ae4a9b8b9df399d003
Ron <ronillo.ang@orangeandbronze.com>
2011-07-13 16:14:24 +0800

    Added technology contraints test.

--- test/unit/com/onb/ozmness/TechnologyTests.groovy
+++ test/unit/com/onb/ozmness/TechnologyTests.groovy
@@ -3,15 +3,31 @@ package com.onb.ozmness
 import grails.test.*
 
 class TechnologyTests extends GrailsUnitTestCase {
-    protected void setUp() {
-        super.setUp()
-    }
 
-    protected void tearDown() {
-        super.tearDown()
-    }
+	void testConstraints() {
+		mockDomain(Technology)
+		def tech = new Technology()
 
-    void testSomething() {
+		tech.name = "Longhorn"
+		mockForConstraintsTests(Technology)
 
-    }
+		println "tech.validate: ${tech.validate()}"
+		println "hasErrors: ${tech.hasErrors()}"
+		tech.errors.allErrors.each { println it }
+		println "tech.save: ${tech.save()}"
+		println "hasErrors: ${tech.hasErrors()}"
+		tech.errors.allErrors.each { println it }
+
+		assert tech.validate()
+		tech = new Technology(name: "Longhorn")
+
+		println "tech.validate: ${tech.validate()}"
+		println "hasErrors: ${tech.hasErrors()}"
+		tech.errors.allErrors.each { println it }
+		println "tech.save: ${tech.save()}"
+		println "hasErrors: ${tech.hasErrors()}"
+		tech.errors.allErrors.each { println it }
+
+		assert tech.validate()
+	}
 }
