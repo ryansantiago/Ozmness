79903ef1e3d955be3bf4d5fb7d48bbde9973563b
Ryan <ryan.santiago@orangeandbronze.com>
2011-07-13 14:16:04 +0800

    Added unit test
    Signed-off-by: Ryan <ryan.santiago@orangeandbronze.com>

--- test/unit/com/onb/ozmness/EmployeePositionTests.groovy
+++ test/unit/com/onb/ozmness/EmployeePositionTests.groovy
@@ -3,24 +3,41 @@ package com.onb.ozmness
 import grails.test.*
 
 class EmployeePositionTests extends GrailsUnitTestCase {
-    protected void setUp() {
-        super.setUp()
-    }
+	protected void setUp() {
+		super.setUp()
+	}
 
-    protected void tearDown() {
-        super.tearDown()
-    }
+	protected void tearDown() {
+		super.tearDown()
+	}
 
-    void testSaveEmployeePosition() {
+	void testSaveEmployeePosition() {
 		mockDomain(EmployeePosition)
 		def pos = new EmployeePosition(name: "CEO")
-		
+
 		assert pos.validate();
 		assert pos.save()
-		
+
 		pos = EmployeePosition.findByName("CEO")
 		assertNotNull pos
-		
+
 		assert pos.validate()
-    }
+	}
+
+	void testDeleteEmployeePosition() {
+		mockDomain(EmployeePosition)
+		def pos = new EmployeePosition(name: "Associate Software Engineer")
+
+		assert pos.validate();
+		assert pos.save()
+
+		pos = EmployeePosition.findByName("Associate Software Engineer")
+		assertNotNull pos
+
+		pos.delete()
+
+		assertNull(EmployeePosition.findByName("Associate Software Engineer"))
+
+		assert pos.validate()
+	}
 }
